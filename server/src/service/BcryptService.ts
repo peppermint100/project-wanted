0f871dc6bc37c3be831158934d12bbe71a2665c0
peppermint100 <krpeppermint100@gmail.com>
2020-07-21 22:47:01 +0900

    jwtService

--- server/src/service/BcryptService.ts
+++ server/src/service/BcryptService.ts
@@ -3,16 +3,17 @@ import bcrypt, { Error, Hash } from "bcrypt"
 const salt = process.env.BCRYPT_SALT || 10;
 
 // encode password
-export const encode = (password: string) => {
+export const encode = (password: string): string | null => {
     bcrypt.hash(password, salt, (err: Error, hash: Hash) => {
         if (err) return null;
-        return hash;
+        return JSON.stringify(hash);
     });
+    return null;
 }
 // decode password
 
-export const compare = (password: string, hashedPassword: string) => {
-    bcrypt.compare(password, hashedPassword, (err: Error, result: boolean) => {
+export const compare = async (password: string, hashedPassword: string) => {
+    await bcrypt.compare(password, hashedPassword, (err: Error, result: boolean) => {
         if (err) return null;
         return result
     })
---
+++ server/src/service/JwtService.ts
@@ -0,0 +1,31 @@
+import jsonwebtoken from "jsonwebtoken"
+
+const SECRET = process.env.JWT_SECRET || "secret"
+
+// create jwt
+
+export const generateToken = (email: string) => {
+    let token = jsonwebtoken.sign(
+        {
+            email,
+        },
+        SECRET,
+        {
+            expiresIn: "1h",
+        }
+    );
+
+    return token;
+};
+
+// validate jwt
+export const verifyToken = (token: string) => {
+    try {
+        let decoded = jsonwebtoken.verify(token, SECRET);
+        return decoded;
+    } catch (TokenExpiredError) {
+        return TokenExpiredError;
+    }
+};
+
+
